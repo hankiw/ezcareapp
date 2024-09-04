8501893d8a4acfa3c1c4dc597bc9fa60da4bb8c9
hankiwoo <hankiwoo@kwic.co.kr>
2024-09-04 17:15:02 +0900

    test

--- ezcareapp/lib/main.dart
+++ ezcareapp/lib/main.dart
@@ -15,7 +15,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primaryColor: Colors.black,
+        canvasColor: Colors.white,
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: HomeScreen(),
--- ezcareapp/lib/screens/home_screen.dart
+++ ezcareapp/lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
-import 'package:ezcareapp/screens/page_main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,24 +11,78 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String webViewUri = 'https://bapdish.com';
+  late WebViewController _controller;
+
+  int _selectedIndex = 0;
+  bool isBottomNavi = true;
+  // String webViewUri = 'https://z05.ezc.kr/page/test/main.html';
+  List<String> webViewUriList = <String> [
+    'https://z05.ezc.kr/page/test/main.html',
+    'https://z05.ezc.kr/page/test/page01.html',
+    'https://z05.ezc.kr/page/test/page02.html',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    
+    late final PlatformWebViewControllerCreationParams params;
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes> {}
+      );
+    } else {
+      params = PlatformWebViewControllerCreationParams();
+    }
+
+    final WebViewController controller = WebViewController.fromPlatformCreationParams(params);
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(Colors.white)
+      ..addJavaScriptChannel(
+        'NativeBottomNavigation',
+        onMessageReceived: (JavaScriptMessage msg) {
+          print(msg.message);
+          bool isShow = (msg.message == 'show') ? true : false;
+          setState(() {
+            isBottomNavi = isShow;
+          });
+        }
+      )
+      ..addJavaScriptChannel(
+        'ToastMessage',
+        onMessageReceived: (JavaScriptMessage msg) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(msg.message),
+              action: SnackBarAction(
+                label: 'close',
+                onPressed: () {}
+              )
+            )
+          );
+        }
+      )
+      ..loadRequest(Uri.parse(webViewUriList[0]));
+
+    _controller = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: CupertinoTabScaffold(
-        backgroundColor: CupertinoColors.white,
-        tabBar: CupertinoTabBar(
-          backgroundColor: CupertinoColors.systemBlue,
-          activeColor: CupertinoColors.white,
-          inactiveColor: CupertinoColors.white.withOpacity(0.5),
-          onTap: (index) {
-            print(index);
-            setState(() {
-              webViewUri = 'https://ezcare.easyms.co.kr';
-            });
-          },
-          items: const [
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('ezcare webview', style: TextStyle(color: CupertinoColors.white)),
+          actions: [
+            ReloadButton(controller: _controller),
+          ],
+        ),
+        backgroundColor: Colors.white,
+        bottomNavigationBar: (isBottomNavi == true) ? BottomNavigationBar(
+          items: const <BottomNavigationBarItem> [
             BottomNavigationBarItem(
               label: '일정&명세서',
               icon: Icon(Icons.calendar_month)
@@ -49,13 +103,35 @@ class _HomeScreenState extends State<HomeScreen> {
               label: '더보기',
               icon: Icon(Icons.more_horiz_rounded)
             ),
-          ]
-        ),
-        tabBuilder: (context, index) {
-          return PageMain(webViewUri: webViewUri);
-        },
+          ],
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.blue,
+          currentIndex: _selectedIndex,
+          selectedItemColor: CupertinoColors.white,
+          unselectedItemColor: CupertinoColors.white.withOpacity(0.5),
+          onTap: _onTapBottomNavi,
+        ) : null,
+        body: WebViewWidget(controller: _controller)
+        // body: Text('hello'),
       ),
     );
+  }
+
+  void _onTapBottomNavi(value) {
+    setState(() {
+      _selectedIndex = value;
+      _controller.loadRequest(Uri.parse(webViewUriList[_selectedIndex]));
+    });
+  }
+}
+
+class ReloadButton extends StatelessWidget {
+  WebViewController controller;
+  ReloadButton({super.key, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(icon: const Icon(Icons.replay), onPressed: () {controller.reload();},);
   }
 }
 
